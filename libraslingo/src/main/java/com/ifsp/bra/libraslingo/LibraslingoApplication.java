7efad16dfa27dcac806e214b115312289756daa3
Ligras <gabigabs2608@gmail.com>
2024-09-20 14:20:59 -0300

    First Commit

---
+++ libraslingo/src/main/java/com/ifsp/bra/libraslingo/LibraslingoApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.bra.libraslingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraslingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraslingoApplication.class, args);
+	}
+
+}
---
+++ libraslingo/src/test/java/com/ifsp/bra/libraslingo/LibraslingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.bra.libraslingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraslingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
