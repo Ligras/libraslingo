cfbada13364511e10a54739bea27e84f9c33711e
ligras <gabigabs2608@gmail.com>
2024-09-26 14:35:34 -0300

    commit jogador controller versao 2

--- libraslingo/src/main/java/com/ifsp/bra/libraslingo/controllers/JogadorController.java
+++ libraslingo/src/main/java/com/ifsp/bra/libraslingo/controllers/JogadorController.java
@@ -1,20 +1,36 @@
 package com.ifsp.bra.libraslingo.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import com.ifsp.bra.libraslingo.model.Jogador;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
+@RequestMapping("/jogadores")
+
 public class JogadorController {
+    private List<Jogador> jogadores = new ArrayList<>();
+    private long ID = 0;
+
+    public JogadorController(){
+        jogadores.add(new Jogador(ID++, "Jogador0", "1234"));
+        jogadores.add(new Jogador(ID++, "Jogador1", "5678"));
+        jogadores.add(new Jogador(ID++, "Jogador2", "4321"));
+
+        
+    }
+
+    //GET sem id
+
     //GET
-
-
-    //GET com ID
-
 
     //POST
 
-
     //PUT
-
 
     //DELETE
 }
+
